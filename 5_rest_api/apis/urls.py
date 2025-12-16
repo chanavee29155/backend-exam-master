3775135f0840a05b6b5be9d1bc43b324cebe4b19
winner <chanavee29155@gmail.com>
2025-12-17 01:38:29 +0700

    api student

--- 5_rest_api/apis/urls.py
+++ 5_rest_api/apis/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from apis.views.v1.school import SchoolViewSet, ClassroomViewSet
 from apis.views.v1.teacher import TeacherViewSet
+from apis.views.v1.student import StudentViewSet
 
 
 router = DefaultRouter()
@@ -10,6 +11,7 @@ router = DefaultRouter()
 router.register(r'school', SchoolViewSet, basename='school')
 router.register(r'classroom', ClassroomViewSet, basename='classroom')
 router.register(r'teacher', TeacherViewSet, basename='teacher')
+router.register(r'student', StudentViewSet, basename='student')
 
 
 api_v1_urls = (router.urls, 'v1')
