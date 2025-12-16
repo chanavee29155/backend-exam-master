70b995dae384562ac8cbdbfbc7c7baa702f77403
winner <chanavee29155@gmail.com>
2025-12-17 00:31:20 +0700

    remove comment

--- 5_rest_api/apis/filters.py
+++ 5_rest_api/apis/filters.py
@@ -3,7 +3,6 @@ import django_filters
 from apis.models import School
 
 
-# code here
 class SchoolFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     
