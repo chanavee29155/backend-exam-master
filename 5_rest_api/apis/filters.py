dd7078624bdf7c8994744396235fd450e3e3f4d3
winner <chanavee29155@gmail.com>
2025-12-17 01:38:16 +0700

    add student view

--- 5_rest_api/apis/filters.py
+++ 5_rest_api/apis/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from apis.models import School, Classroom, Teacher
+from apis.models import School, Classroom, Teacher, Student
 
 
 class SchoolFilter(django_filters.FilterSet):
@@ -42,3 +42,25 @@ class TeacherFilter(django_filters.FilterSet):
     
     def filter_classroom(self, queryset, name, value):
         return queryset.filter(classrooms__id=value)
+
+
+class StudentFilter(django_filters.FilterSet):
+    school = django_filters.CharFilter(method='filter_school', label='school')
+    classroom = django_filters.CharFilter(method='filter_classroom', label='classroom')
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    last_name = django_filters.CharFilter(lookup_expr='icontains')
+    gender = django_filters.ChoiceFilter(choices=[
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ])
+    
+    class Meta:
+        model = Student
+        fields = ('school', 'classroom', 'first_name', 'last_name', 'gender')
+    
+    def filter_school(self, queryset, name, value):
+        return queryset.filter(classroom__school__name__icontains=value)
+    
+    def filter_classroom(self, queryset, name, value):
+        return queryset.filter(classroom__id=value)
--- 5_rest_api/apis/serializers.py
+++ 5_rest_api/apis/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apis.models import School, Classroom, Teacher
+from apis.models import School, Classroom, Teacher, Student
 
 
 class SchoolSerializer(serializers.ModelSerializer):
@@ -46,13 +46,11 @@ class TeacherSerializer(serializers.ModelSerializer):
         first_name = attrs.get('first_name')
         last_name = attrs.get('last_name')
         
-        # Check if teacher with same first_name and last_name already exists
         queryset = Teacher.objects.filter(
             first_name=first_name,
             last_name=last_name
         )
         
-        # If updating existing teacher, exclude current instance
         if self.instance is not None:
             queryset = queryset.exclude(id=self.instance.id)
         
@@ -60,6 +58,37 @@ class TeacherSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 {
                     'detail': f'Teacher with name "{first_name} {last_name}" already exists'
+                }
+            )
+        
+        return attrs
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(read_only=True)
+    classroom_detail = ClassroomSerializer(source='classroom', read_only=True)
+    
+    class Meta:
+        model = Student
+        fields = '__all__'
+    
+    def validate(self, attrs):
+        first_name = attrs.get('first_name')
+        last_name = attrs.get('last_name')
+        
+        queryset = Student.objects.filter(
+            first_name=first_name,
+            last_name=last_name
+        )
+        
+        if self.instance is not None:
+            queryset = queryset.exclude(id=self.instance.id)
+        
+        if queryset.exists():
+            existing_student = queryset.first()
+            raise serializers.ValidationError(
+                {
+                    'detail': f'Student with name "{first_name} {last_name}" already exists in {existing_student.classroom}'
                 }
             )
         
